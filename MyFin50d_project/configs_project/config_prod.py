b11f1ad4935a8d90d5c7bd9482102b4e34d32f70
thebuleganteng <mcdonnell.matthew@ymail.com>
2024-03-27 17:40:51 +0700

    -

---
+++ MyFin50d_project/configs_project/config_prod.py
@@ -0,0 +1,9 @@
+import os
+
+CSS_CACHE_ENABLED = True
+DEBUG = False # Debug must be false for prod
+
+# Logging configurations
+LOG_TO_CONSOLE = False
+LOG_TO_FILE = False
+USE_GOOGLE_CLOUD_LOGGING = True
