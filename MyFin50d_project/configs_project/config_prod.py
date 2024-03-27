5a2c89f7c636487778609855211e5d0170a269eb
thebuleganteng <mcdonnell.matthew@ymail.com>
2024-03-27 17:40:18 +0700

    More issues!

--- MyFin50d_project/configs_project/config_prod.py
+++
@@ -1,9 +0,0 @@
-import os
-
-CSS_CACHE_ENABLED = True
-DEBUG = False # Debug must be false for prod
-
-# Logging configurations
-LOG_TO_CONSOLE = False
-LOG_TO_FILE = False
-USE_GOOGLE_CLOUD_LOGGING = True
